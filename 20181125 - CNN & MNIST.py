dfbd2bb64a21fc3fa385a8d8ff85b25ffea28237
surface <surface@chenxy.tech>
2018-11-25 16:34:30 +0800

    [L2][CNN] CNN Model learning rate reduction, dataset manipulation

--- 20181125 - CNN & MNIST.py
+++ 20181125 - CNN & MNIST.py
@@ -140,6 +140,74 @@ g = plt.imshow(X_test[choice][:,:,0], cmap=plt.cm.binary)
 # #### Saving simple model:
 
 #%%
-simple_model.save("C:/Users/chenx/Desktop/1811_softdesign/20181125 - MNIST/simple.model")
+## simple_model.save("C:/Users/chenx/Desktop/1811_softdesign/20181125 - MNIST/simple.model")
 
 #%% [markdown]
+# CNN Model:
+
+#%%
+cnn_model = Sequential()
+
+cnn_model.add(Conv2D(filters = 32,
+                    kernel_size = (5,5),
+                    padding = 'Same',
+                    activation = 'relu'))
+
+cnn_model.add(Conv2D(filters = 32,
+                    kernel_size = (5,5),
+                    padding = 'Same',
+                    activation = 'relu'))
+
+cnn_model.add(MaxPool2D(pool_size=(2, 2)))
+cnn_model.add(Dropout(0.25))
+
+
+cnn_model.add(Conv2D(filters = 64,
+                    kernel_size = (3, 3),
+                    padding = 'Same',
+                    activation = 'relu'))
+
+cnn_model.add(Conv2D(filters = 64,
+                    kernel_size = (3, 3),
+                    padding = 'Same',
+                    activation = 'relu'))
+
+cnn_model.add(MaxPool2D(pool_size=(2, 2),
+                        strides=(2, 2)))
+cnn_model.add(Dropout(0.25))
+
+cnn_model.add(Flatten())
+cnn_model.add(Dense(256, activation='relu'))
+cnn_model.add(Dropout(0.5))
+# Output Layer
+cnn_model.add(Dense(10, activation='softmax'))
+
+#%%
+cnn_model.compile(optimizer=RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+#%%
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+#%%
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+datagen.fit(X_train)
+
+#%%
